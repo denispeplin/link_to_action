613873cb13a9d02b6d784537d5b6121c2c217a00
denispeplin <denis.peplin@gmail.com>
2012-09-27 13:12:01 +0400

    Add link_to_new helper.

---
+++ lib/link_to_action.rb
@@ -0,0 +1,28 @@
+require "link_to_action/version"
+require 'action_view'
+require 'active_model'
+
+module LinkToAction
+  def link_to_new(object, options = {})
+    name = options.delete(:name) || t_action(object, :create)
+    params = options.delete(:params) || {}
+    params[:action] = :new
+    link_to name, polymorphic_url(object, params), options
+  end
+
+  private
+
+  # TODO: inspect some advanced I18n
+  # actionpack/lib/action_view/helpers/form_helper.rb, submit_default_value
+  def t_action(object, action)
+    model = if object.respond_to?(:model_name)
+      object.model_name.human
+    else
+      object.class.model_name.human
+    end
+
+    t(:"helpers.submit.#{action}", model: model)
+  end
+end
+
+ActionView::Base.send :include, LinkToAction
