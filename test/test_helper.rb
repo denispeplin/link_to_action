86b8a8f83339121c80dbdde46d22074220a1be42
denispeplin <denis.peplin@gmail.com>
2012-10-02 13:40:10 +0400

    Include LinkToAction::Helpers into ActionView::TestCase

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
+require "link_to_action"
+
 Rails.backtrace_cleaner.remove_silencers!
 
 # Load support files
@@ -12,4 +14,8 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 # Load fixtures from the engine
 if ActiveSupport::TestCase.method_defined?(:fixture_path=)
   ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
+end
+
+class ActionView::TestCase
+  include LinkToAction::Helpers
 end
