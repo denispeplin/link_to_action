f35a89bf71f01bee14b4f4d5d05734758e2718cc
denispeplin <denis.peplin@gmail.com>
2012-10-02 16:12:09 +0400

    Change order, no content changes.

--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class HelperTest < ActionView::TestCase
-  test 'link_to_back first time' do
-    assert_equal "<a href=\"javascript:history.back()\">Back</a>",
-      link_to_back
-  end
-
   test 'link_to_new' do
     assert_equal "<a href=\"/users/new\">New User</a>",
       link_to_new(User), '/path'
+  end
+
+  test 'link_to_back first time' do
+    assert_equal "<a href=\"javascript:history.back()\">Back</a>",
+      link_to_back
   end
   
   test 'link_to_back first time with classes' do
