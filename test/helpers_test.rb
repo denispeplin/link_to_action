81dd93c2b2c9d42ed04f1acaad276acfe5b2982e
denispeplin <denis.peplin@gmail.com>
2012-10-02 14:13:22 +0400

    Test link_to_back with and without using classes.

--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class HelperTest < ActionView::TestCase
-  test 'link_to_back returns javascript for the first time' do
+  test 'link_to_back first time' do
+    assert_equal "<a href=\"javascript:history.back()\">Back</a>",
+      link_to_back
+  end
+
+  LinkToAction.use_classes
+  test 'link_to_back first time with classes' do
+    LinkToAction.use_classes = true
     assert_equal "<a href=\"javascript:history.back()\" class=\"btn\">Back</a>",
       link_to_back
   end
