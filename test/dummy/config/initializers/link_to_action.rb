068f20e47617985fe2bf7c6b9868ee1f5e06f7d3
denispeplin <denis.peplin@gmail.com>
2012-10-03 12:07:35 +0400

    Add initializer for link_to_action to dummy, all default.

---
+++ test/dummy/config/initializers/link_to_action.rb
@@ -0,0 +1,34 @@
+LinkToAction.setup do |config|
+  # Use CanCan, default false
+  # config.use_cancan = false
+
+  # Add icons to links, default false
+  # config.use_icons = false
+
+  # Icons size, default 'large'
+  # config.icons_size = 'large'
+
+  # Icons for actions
+  # config.icon_new = 'plus'
+  # config.icon_edit = 'edit'
+  # config.icon_destroy = 'trash'
+  # config.icon_back = 'undo'
+
+  # Use classes, default false
+  # config.use_classes = false
+
+  # Classes by default are adopted to twitter-bootstrap
+  # config.class_default = 'btn'
+
+  # Classes for actions
+  # config.class_new = 'btn-primary'
+  # config.class_edit = nil
+  # config.class_destroy = 'btn-danger'
+  # config.class_back = nil
+
+  # Size can be set using class, by default is adopted to twitter-bootstrap
+  # config.size_class_default = nil
+  # config.size_class_large = 'btn-large'
+  # config.size_class_small = 'btn-small'
+  # config.size_class_mini = 'btn-mini'
+end
