48208f40d0378f9e03c9d13807018b64082ae74a
denispeplin <denis.peplin@gmail.com>
2012-10-03 14:58:47 +0400

    Translate 'User' -> 'My User' for better I18n test.

--- test/helpers_test.rb
+++ test/helpers_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class HelperTest < ActionView::TestCase
   test 'link_to_new' do
-    assert_equal "<a href=\"/users/new\">New User</a>",
+    assert_equal "<a href=\"/users/new\">New My User</a>",
       link_to_new(User)
   end
 
@@ -13,11 +13,11 @@ class HelperTest < ActionView::TestCase
   end
 
   test 'link_to_edit' do
-    assert_equal "<a href=\"/users/1/edit\">Edit User</a>", link_to_edit(@user)
+    assert_equal "<a href=\"/users/1/edit\">Edit My User</a>", link_to_edit(@user)
   end
 
   test 'link_to_destroy' do
-    assert_equal "<a href=\"/users/1\" data-confirm=\"Are you sure?\" data-method=\"delete\" data-skip-pjax=\"true\" rel=\"nofollow\">Delete User</a>",
+    assert_equal "<a href=\"/users/1\" data-confirm=\"Are you sure?\" data-method=\"delete\" data-skip-pjax=\"true\" rel=\"nofollow\">Delete My User</a>",
       link_to_destroy(@user)
   end
 
