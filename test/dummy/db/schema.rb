074655c6f30da6a49d34153893df2e7ede7f440a
Denis <denis.peplin@gmail.com>
2012-10-04 23:28:08 -0400

    Add login to dummy app user

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121003103958) do
+ActiveRecord::Schema.define(:version => 20121005032141) do
 
   create_table "comments", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20121003103958) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "login"
   end
 
 end
