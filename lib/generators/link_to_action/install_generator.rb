0c82916907b2ac685e1761dc739679d2b11e951d
Denis Peplin <denis.peplin@gmail.com>
2012-10-20 18:36:08 +0400

    Copy templates upon install.

--- lib/generators/link_to_action/install_generator.rb
+++ lib/generators/link_to_action/install_generator.rb
@@ -5,16 +5,24 @@ module LinkToAction
   module Generators
     class InstallGenerator < Rails::Generators::Base
       desc "Copy LinkToAction configuration file"
-      source_root File.expand_path('../../..', __FILE__)
+      source_root File.expand_path('../templates', __FILE__)
 
       def copy_initializers
-        copy_file 'generators/link_to_action/templates/link_to_action.rb',
-          'config/initializers/link_to_action.rb'
+        copy_file 'link_to_action.rb', 'config/initializers/link_to_action.rb'
       end
 
       def copy_locale_file
-        copy_file 'link_to_action/locale/en.yml',
+        copy_file '../../../link_to_action/locale/en.yml',
           'config/locales/link_to_action.en.yml'
+      end
+      
+      def copy_templates
+        # TODO: one LOC
+        copy_file '_form.html.erb', 'lib/templates/erb/scaffold/_form.html.erb'
+        copy_file 'edit.html.erb', 'lib/templates/erb/scaffold/edit.html.erb'
+        copy_file 'index.html.erb', 'lib/templates/erb/scaffold/index.html.erb'
+        copy_file 'new.html.erb', 'lib/templates/erb/scaffold/new.html.erb'
+        copy_file 'show.html.erb', 'lib/templates/erb/scaffold/show.html.erb'
       end
     end
   end
