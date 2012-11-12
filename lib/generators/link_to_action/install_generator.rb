91f2d426ef6710b5d02828155ff2f9376703d4fd
Denis Peplin <denis.peplin@gmail.com>
2012-11-12 20:58:23 +0400

    Add --bootstrap option to install generator.

--- lib/generators/link_to_action/install_generator.rb
+++ lib/generators/link_to_action/install_generator.rb
@@ -6,9 +6,23 @@ module LinkToAction
     class InstallGenerator < Rails::Generators::Base
       desc "Copy LinkToAction configuration file"
       source_root File.expand_path('../templates', __FILE__)
+      class_option :bootstrap, type: :boolean,
+        desc: 'Add the Twitter Bootstrap option and templates.'
+
+      def info_framework
+        puts "link_to_action supports Twitter Bootstrap. If you want a " \
+          "configuration that is compatible with this framework, then please " \
+          "re-run this generator with --bootstrap option." unless options.bootstrap?
+      end
 
       def copy_initializers
         copy_file 'link_to_action.rb', 'config/initializers/link_to_action.rb'
+        if options[:bootstrap]
+          gsub_file 'config/initializers/link_to_action.rb',
+            %r|# config.use_icons = false|, 'config.use_icons = true'
+          gsub_file 'config/initializers/link_to_action.rb',
+            %r|# config.use_classes = false|, 'config.use_classes = true'
+        end
       end
 
       def copy_locale_file
